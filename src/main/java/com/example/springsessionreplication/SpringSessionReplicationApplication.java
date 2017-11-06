78a93daf6bbcd3e042d96061ea053c200ae96146
HyungCheol Kim <hcgoon+test@gmail.com>
2017-11-06 18:01:26 +0900

    Initial commit

---
+++ src/main/java/com/example/springsessionreplication/SpringSessionReplicationApplication.java
@@ -0,0 +1,12 @@
+package com.example.springsessionreplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionReplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSessionReplicationApplication.class, args);
+	}
+}
